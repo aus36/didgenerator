ec382ea55c3e0675be3d629dd5013f96efd8ad39
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-07-31 02:37:10 -0500

    Added VP creation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { sha512 } from '@noble/hashes/sha512';
 import useSound from 'use-sound';
 
 function generateDIDDocument(url) {
-  // Generate a sha idk what that means
+  // Generate sha 512 sync
   ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
 
   // Generate a key pair
@@ -48,6 +48,137 @@ function generateDIDDocument(url) {
   return didDoc;
 }
 
+function generateVP(url) {
+
+  // generate web DID from url
+  let id = "did:web:" + String(url);
+
+  let vp =
+  {
+    "@context": [
+      "https://www.w3.org/2018/credentials/v1",
+      "https://www.w3.org/2018/credentials/examples/v1"
+    ],
+    "id": "urn:uuid:3978344f-8596-4c3a-a978-8fcaba3903c5",
+    "type": [
+      "VerifiablePresentation",
+      "CredentialManagerPresentation"
+    ],
+    "verifiableCredentials": [
+      {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://www.w3.org/2018/credentials/examples/v1"
+        ],
+        "id": "http://example.edu/credentials/58473",
+        "type": [
+          "VerifiableCredential"
+        ],
+        "issuer": "https://tamuc.edu/issuers/565049",
+        "issuanceDate": "2021-01-01T00:00:00Z",
+        "expirationDate": "2021-01-01T00:00:00Z",
+        "credentialSubject": {
+          "id": id,
+          "type": [
+            "eldest"
+          ],
+          "prev": "null",
+          "seqno": 0,
+          "tag": "signature"
+        },
+        "credentialStatus": {
+          "id": "https://example.edu/status/24",
+          "type": "CredentialStatusList2017"
+        },
+        "proof": {
+          "type": "Ed25519VerificationKey2020",
+          "created": "2022-02-25T14:58:43Z",
+          "verificationMethod": "https://example.edu/issuers/14#key-1",
+          "proofPurpose": "assertionMethod",
+          "proofValue": ""
+        }
+      },
+      {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://www.w3.org/2018/credentials/examples/v1"
+        ],
+        "id": "http://example.edu/credentials/58473",
+        "type": [
+          "VerifiableCredential"
+        ],
+        "issuer": "https://tamuc.edu/issuers/565049",
+        "issuanceDate": "2021-01-01T00:00:00Z",
+        "expirationDate": "2021-01-01T00:00:00Z",
+        "credentialSubject": {
+          "id": id,
+          "type": [
+            "web_service_binding"
+          ],
+          "service": {
+            "name": "github", 
+            "username": "exampleStudent"
+          },
+          "prev": "sre54g...",
+          "seqno": 1,
+          "tag": "signature"
+        },
+        "credentialStatus": {
+          "id": "https://example.edu/status/24",
+          "type": "CredentialStatusList2017"
+        },
+        "proof": {
+          "type": "Ed25519VerificationKey2020",
+          "created": "2022-02-25T14:58:43Z",
+          "verificationMethod": "https://example.edu/issuers/14#key-1",
+          "proofPurpose": "assertionMethod",
+          "proofValue": ""
+        }
+      },
+      {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://www.w3.org/2018/credentials/examples/v1"
+        ],
+        "id": "http://example.edu/credentials/58473",
+        "type": [
+          "VerifiableCredential"
+        ],
+        "issuer": "https://tamuc.edu/issuers/565049",
+        "issuanceDate": "2021-01-01T00:00:00Z",
+        "expirationDate": "2021-01-01T00:00:00Z",
+        "credentialSubject": {
+          "id": id,
+          "type": "revoke",
+          "revoke": {
+            "sig_ids": [ "038cd…", "f927c…" ]
+          },
+          "prev": "sre54g...",
+          "seqno": 2,
+          "tag": "signature"
+        },
+        "credentialStatus": {
+          "id": "https://example.edu/status/24",
+          "type": "CredentialStatusList2017"
+        },
+        "proof": {
+          "type": "Ed25519VerificationKey2020",
+          "created": "2022-02-25T14:58:43Z",
+          "verificationMethod": "https://example.edu/issuers/14#key-1",
+          "proofPurpose": "assertionMethod",
+          "proofValue": ""
+        }
+      }
+    ]
+  }
+
+  // output vp to console
+  console.log("");
+  console.log(JSON.stringify(vp, null, 2));
+
+  // return vp back to app
+  return vp;
+}
 
 function App() {
   // useState to store the url
@@ -66,7 +197,7 @@ function App() {
           </label>
           <button type="submit">Submit form (actually refresh lol)</button>
         </form>
-        <button onClick={() => { setDidDoc(generateDIDDocument(url)); setLoaded(true); }}>Generate DID Document</button>
+        <button onClick={() => { setDidDoc(generateDIDDocument(url)); setLoaded(true); generateVP(url)}}>Generate DID Document and VP</button>
         <code style={{ margin: 20 }}>
           {loaded
             ? JSON.stringify(didDoc, null, 2)
