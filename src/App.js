9a94d360b59dd93d61760de6e3ec9d634b7d90cd
Austin Hale <93340060+aus36@users.noreply.github.com>
2023-07-30 23:30:26 -0500

    Added useState for url and first form field

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import maoi from './maoi.jpeg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // useState to store the url
+  const [url, setUrl] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={maoi} className="App-logo" alt="logo"/>
+        <form style={{margin:10, display:"flex", flexDirection: 'column'}}>
+          <input onChange={setUrl} placeholder='enter your did url here:'>
+          </input>
+          There will be fields here to enter info for the did
+        </form>
+        <button style = {{margin:10}}>
+          A button here will generate a did
+        </button>
       </header>
     </div>
   );
